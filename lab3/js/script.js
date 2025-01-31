566465f9659ba199b2e149fee88b825726971d5a
michellecong <cong.m@northeastern.edu>
2025-01-30 21:01:28 -0800

    price for each item

---
+++ lab3/js/script.js
@@ -0,0 +1,18 @@
+// Prices for each item
+const prices = {
+  flavor: {
+    original: 2.5,
+    mango: 3.0,
+    strawberry: 3.5,
+  },
+  size: {
+    small: 1.0,
+    medium: 1.5,
+    large: 2.0,
+  },
+  toppings: {
+    boba: 0.5,
+    jelly: 0.75,
+    pudding: 1.0,
+  },
+};
