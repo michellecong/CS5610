1eaf8074434c61514b16b3a22a4249796f30e6e0
michellecong <cong.m@northeastern.edu>
2025-02-07 16:08:33 -0800

    modify functions of place order

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -17,6 +17,12 @@ const prices = {
   },
 };
 
+const flavor = document.getElementById("flavor").value;
+const size = document.getElementById("size").value;
+const toppings = Array.from(
+  document.querySelectorAll('input[name="topping"]:checked')
+).map((topping) => topping.value);
+
 function displayOrderSummary(order) {
   console.log(
     `You have ordered a ${order.size} ${
@@ -26,37 +32,43 @@ Total Price: $${order.finalPrice.toFixed(2)}.`
   );
 }
 
-function placeOrder(flavor, size, toppings) {
-  const basePrice = prices.flavor[flavor];
-  const sizeMultiplier = prices.size[size];
-  const toppingsPrice = toppings.reduce(
-    (acc, topping) => acc + prices.toppings[topping],
-    0
-  );
-  const finalPrice = sizeMultiplier * (basePrice + toppingsPrice);
-  let order = {
-    flavor: flavor,
-    size: size,
-    toppings: toppings,
-    finalPrice: finalPrice,
-  };
+function placeOrder() {
+  if (!validateOrder()) {
+    alert("Please select a flavor and size.");
+    return;
+  } else {
+    const basePrice = prices.flavor[flavor];
+    const sizeMultiplier = prices.size[size];
+    const toppingsPrice = toppings.reduce(
+      (acc, topping) => acc + prices.toppings[topping],
+      0
+    );
 
-  displayOrderSummary(order);
+    const finalPrice = sizeMultiplier * (basePrice + toppingsPrice);
+
+    let order = {
+      flavor: flavor,
+      size: size,
+      toppings: toppings,
+      finalPrice: finalPrice,
+    };
+
+    displayOrderSummary(order);
+  }
 }
 
-function testOrder() {
-  const flavor = "original";
-  const size = "medium";
-  const toppings = ["boba", "jelly"];
-  placeOrder(flavor, size, toppings);
-}
+// function testOrder() {
+//   const flavor = "original";
+//   const size = "medium";
+//   const toppings = ["boba", "jelly"];
+//   placeOrder(flavor, size, toppings);
+// }
 
 // Function to validate if the required fields are filled out
 function validateOrder() {
   const flavor = document.getElementById("flavor").value;
   const size = document.getElementById("size").value;
   if (!flavor || !size) {
-    alert("Please select a flavor and size.");
     return false;
   }
   return true;
