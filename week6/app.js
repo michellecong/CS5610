a11d85a4c99771c1c03abad2f2f5430fb53f4e62
michellecong <cong.m@northeastern.edu>
2025-02-13 11:42:08 -0800

    add file reading

--- week6/app.js
+++ week6/app.js
@@ -2,13 +2,21 @@
 const fs = require("fs");
 // console.log(fs); // { writeFileSync: [Function: writeFileSync], ... }
 
-function writeCB() {
-  console.log("write file done");
-}
+// function writeCB() {
+//   console.log("write file done");
+// }
 fs.writeFileSync("data.txt", "Hello, World!", (err) => {
   if (err) {
     console.log("write failed");
   } else {
     console.log("write file done");
+  }
+});
+
+fs.readFile("data.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("read failed");
+  } else {
+    console.log(data);
   }
 });
