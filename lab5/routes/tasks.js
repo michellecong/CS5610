126c9f5ca11fedb27308229fc761352935351984
michellecong <cong.m@northeastern.edu>
2025-02-22 14:06:33 -0800

    respond with all the needed data in json

--- lab5/routes/tasks.js
+++ lab5/routes/tasks.js
@@ -19,15 +19,24 @@ router.get("/", async (req, res) => {
 router.get("/:taskId", async (req, res) => {
   try {
     const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos/${req.params.taskId}`
+    );
+
+    const response2 = await axios.get(
       `https://jsonplaceholder.typicode.com/users/${req.params.taskId}`
     );
 
-    res.json(response.data.name);
-
+    res.json({
+      id: req.params.taskId,
+      title: response.data.title,
+      completed: response.data.completed,
+      name: response2.data.name,
+    });
     // res.render("task", {
     //   id: req.params.taskId,
     //   title: response.data.title,
     //   completed: response.data.completed,
+    //   name: response2.data.name,
     // });
   } catch (error) {
     res.json({ message: error.message });
