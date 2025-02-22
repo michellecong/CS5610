c748fc12ce3ce277d17fea6924f9c0677a14b415
michellecong <cong.m@northeastern.edu>
2025-02-22 13:44:57 -0800

    add router to app

--- lab5/app.js
+++ lab5/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-const port = 3000;
+const taskRouter = require("./routes/tasks.js");
 
+app.use("/tasks", taskRouter);
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
