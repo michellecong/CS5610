f3e9afee4bada0bec570a0c8893a9e8d09bb95ea
michellecong <cong.m@northeastern.edu>
2025-02-22 13:53:35 -0800

    handle the taskId and get the jason data

--- lab5/routes/tasks.js
+++ lab5/routes/tasks.js
@@ -3,3 +3,33 @@ const router = express.Router();
 const axios = require("axios");
 
 module.exports = router;
+
+router.get("/", async (req, res) => {
+  // try {
+  //   const response = await axios.get(
+  //     "https://jsonplaceholder.typicode.com/todos/"
+  //   );
+  //   console.log(response.data);
+  //   res.json(response.data);
+  // } catch (error) {
+  //   res.json({ message: error.message });
+  // }
+});
+
+router.get("/:taskId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${req.params.taskId}`
+    );
+
+    res.json(response.data);
+
+    // res.render("task", {
+    //   id: req.params.taskId,
+    //   title: response.data.title,
+    //   completed: response.data.completed,
+    // });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
