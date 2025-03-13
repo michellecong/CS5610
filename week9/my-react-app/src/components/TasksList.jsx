067dc26db02ab1dd5c01693505734ca364e63488
michellecong <cong.m@northeastern.edu>
2025-03-13 12:48:07 -0700

    setTasks at initial

--- week9/my-react-app/src/components/TasksList.jsx
+++ week9/my-react-app/src/components/TasksList.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { use } from "react";
 import Task from "./Task";
+import { useState } from "react";
 
 const tasks = [
   {
@@ -20,11 +21,30 @@ const tasks = [
 ];
 
 export default function TasksList() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Review week 9 material",
+      date: "June 4th at 1 pm",
+    },
+    {
+      id: 2,
+      title: "Do quiz 9",
+      date: "June 4th at 6 pm",
+    },
+    {
+      id: 3,
+      title: "Work on assignment 2",
+      date: "June 5th at 8 am",
+    },
+  ]);
+  // setTasks([]);
+  // console.log(tasks);
   return (
     <>
       <ul>
         {tasks.map((task) => {
-          return <Task taskObj={task} />;
+          return <Task key={task.id} taskObj={task} />;
         })}
       </ul>
     </>
