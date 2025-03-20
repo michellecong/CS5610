90f85eeb4706ce1acfb859283f73593c02e78544
michellecong <cong.m@northeastern.edu>
2025-03-20 12:37:16 -0700

    connect data to task list

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -2,12 +2,21 @@ import React, { use } from "react";
 import Header from "./components/Header";
 import TasksList from "./components/TasksList";
 import AddTask from "./components/AddTask";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [tasksFromServer, setTasksFromServer] = useState([]);
   async function fetchData() {
     try {
       const response = fetch("http://localhost:3001/tasks");
-      const data = await response.json();
+
+      if (response.ok) {
+        const data = await response.json();
+        setTasksFromServer(data);
+        console.log(data);
+      } else {
+        throw new Error("Something went wrong");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -23,7 +32,7 @@ export default function App() {
     <div className="App-container">
       <Header myAppName={appName} />
       <AddTask />
-      <TasksList />
+      <TasksList tasks={tasksFromServer} />
     </div>
   );
 }
